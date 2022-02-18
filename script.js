7ce511e935efdebfc0cb6e61b2b9c8601cea854b
masammasa <afbteamx@gmail.com>
2022-02-18 15:55:34 +0900

    モーダルとreset.css追加

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ $(function(){
         // $('onText_p').append('<p class = "ontext_p">テキスト</p>');
 
     })
+    $('.js-modal-open').each(function () {
+        $(this).on('click', function () {
+          var target = $(this).data('target');
+          var modal = document.getElementById(target);
+          $(modal).fadeIn();
+          return false;
+        });
+      });
+      $('.js-modal-close').on('click', function () {
+        $('.js-modal').fadeOut();
+        return false;
+      })
 
 })
