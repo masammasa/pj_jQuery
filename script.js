0d08f05bd3c92a48ca61e7d381c98dfc562dd691
大谷 凌也 <>
2022-02-18 16:12:39 +0900

    メインイメージ変えました

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $(function(){
     console.log("test");
-    $('.onText').on('mouseover',function(){
+    //マウスオーバーで文字を出す
+    // $('.onText').on('mouseover',function(){
+    //     $('#topImg').css('opacity',0.3);
+    //     $('.onText').append('<h1 class = ”mouseoverText”>テキスト</h1>');
+    //     console.log('.”mouseoverText”');
+    // })
+
+
+    //マウスリーブで元に戻す
+    // $('.onText').on('mouseleave',function(){
+    //     $('#topImg').css('opacity','');
+    // })
         // var testText =document.createElement("p");
         // testText.classList.add("test");
         // testText.textContent ="test"
         // $(".onText").append(testText);
         // $('onText_p').append('<p class = "ontext_p">テキスト</p>');
-
-    })
-
+   
+    
 })
