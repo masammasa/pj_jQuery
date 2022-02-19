556b784ebe07accd9f976afc18785bbd0d447c67
masammasa <afbteamx@gmail.com>
2022-02-19 12:49:01 +0900

    メニュー追加

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-$(function(){
-    console.log("test");
-    $('.onText').on('mouseover',function(){
-        // var testText =document.createElement("p");
-        // testText.classList.add("test");
-        // testText.textContent ="test"
-        // $(".onText").append(testText);
-        // $('onText_p').append('<p class = "ontext_p">テキスト</p>');
-    })
-    $('.js-modal-open').each(function () {
-        $(this).on('click', function () {
-          var target = $(this).data('target');
-          var modal = document.getElementById(target);
-          $(modal).fadeIn();
-          return false;
-        });
-      });
-      $('.js-modal-close').on('click', function () {
-        $('.js-modal').fadeOut();
-        return false;
-      })
+$(function () {
+  $('.onText').on('mouseover', function () {
+    // var testText =document.createElement("p");
+    // testText.classList.add("test");
+    // testText.textContent ="test"
+    // $(".onText").append(testText);
+    // $('onText_p').append('<p class = "ontext_p">テキスト</p>');
+  })
+  $('.js-modal-open').each(function () {
+    $(this).on('click', function () {
+      var target = $(this).data('target');
+      var modal = document.getElementById(target);
+      $(modal).fadeIn();
+      return false;
+    });
+  });
+  $('.js-modal-close').on('click', function () {
+    $('.js-modal').fadeOut();
+    return false;
+  })
+  $(document).ready(function () {
+    $('.drawer').drawer({
+      class: {
+        nav: 'drawer-nav',
+        toggle: 'drawer-toggle',
+        overlay: 'drawer-overlay',
+        open: 'drawer-open',
+        close: 'drawer-close',
+        dropdown: 'drawer-dropdown'
+      },
+      iscroll: {
+        // Configuring the iScroll
+        // https://github.com/cubiq/iscroll#configuring-the-iscroll
+        mouseWheel: true,
+        preventDefault: false
+      },
+      showOverlay: true
+    });
+  });
 
 
 
@@ -57,13 +75,13 @@ $(function(){
 
 
 
-// アコーディオン
-$('.question').on('click',function(){
+  // アコーディオン
+  $('.question').on('click', function () {
     $(this).next().slideToggle();
     $(this).toggleClass('on')
-})
+  })
 
-    $(".topBtn").on("click",function(){
-      $("body,html").animate({"scrollTop":0},1000)
-    })
+  $(".topBtn").on("click", function () {
+    $("body,html").animate({ "scrollTop": 0 }, 1000)
+  })
 })
