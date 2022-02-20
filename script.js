44aed5e8342b01e17502d85f7b61cf48c4df7881
大谷 凌也 <>
2022-02-20 19:16:22 +0900

    ロードイベント足しました

--- script.js
+++ script.js
@@ -103,3 +103,9 @@ $(function () {
     $("body,html").animate({ "scrollTop": 0 }, 1000)
   })
 })
+
+//logoの表示
+$(window).on('load',function(){
+  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+});
